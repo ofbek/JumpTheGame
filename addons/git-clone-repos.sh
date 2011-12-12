0cc796b19132e5633a55e5775f3f2409d8f59996
marieke verbiesen <marieke@marieke.nu>
2011-12-13 00:23:17 +0100

    added git clone script

---
+++ addons/git-clone-repos.sh
@@ -0,0 +1,6 @@
+git clone http://github.com/companje/ofxAssets
+git clone http://github.com/vanderlin/ofxBox2d
+git clone http://github.com/companje/ofxExtras
+git clone http://github.com/companje/ofxIniSettings
+git clone http://github.com/roxlu/ofxOpenNI
+git clone http://github.com/companje/ofxSpriteManager
